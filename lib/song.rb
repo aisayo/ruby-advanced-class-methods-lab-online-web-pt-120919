ee5c3ed836b8aed02c8cd00f32d89d76545b88e7
Aysan Isayo <aisayo@icloud.com>
2020-01-14 23:39:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,17 @@
 class Song
   
+  @@all = []
+  
   #method name create
   #class method 
   
   def self.create
     #instantiate a Song
       new_obj = self.new
+      
     #save the song 
+      @@all << self 
+    
     #return the song 
   end 
 
